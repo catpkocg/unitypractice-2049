e9f7ae13263670bc64cbc6df8f15879ffbff2761
pkocg <catpkocg@gmail.com>
2022-08-21 11:53:43 +0900

    ewqrerweqr

--- 2049Practice/Assets/Scripts/GameManager.cs
+++ 2049Practice/Assets/Scripts/GameManager.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private int width = 8;
-    [SerializeField]
-    private int height = 8;
-    [SerializeField]
-    private Node nodePrefab;
-    
+    [SerializeField] private int width = 8;
+    [SerializeField] private int height = 8;
+    [SerializeField] private Node nodePrefab;
+
     public GameObject[] Blocks;
     public List<Node> Nodes;
     public int[,] Array = new int [8, 8];
@@ -19,12 +17,11 @@ public class GameManager : MonoBehaviour
 
     public GameObject BASE;
 
-    private List<Vector3> GoodPos;
-
-
-
+    private Vector3 Pos;
     private Vector2[] BlockShape;
-    
+
+    public List<Vector3> Good = new List<Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,23 +31,25 @@ public class GameManager : MonoBehaviour
         Array[4, 3] = 1;
         //초기 array 값은 모두 0;
         // block 위치대로 array 값 1 주기.
-        
+
         //블락별로 index 다르게 주기
         //점수에 영향
-        
+
         GenerateGrid();
         Debug.Log(Nodes[1].Pos);
         Debug.Log(Nodes[2].Pos);
         Debug.Log(Nodes[64].Pos);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            var Cute = Blocks[0].GetComponent<Block>().ShapePos;
-            if (!CanSpawn(Cute))
+            int random = Random.Range(0, Blocks.Length);
+            var RandomBlock = Blocks[random].GetComponent<Block>().ShapePos;
+            
+            if (CanSpawn(RandomBlock))
             {
                 GameOver();
             }
@@ -58,9 +57,10 @@ public class GameManager : MonoBehaviour
             {
                 SpawnBlock();
             }
-            
+
         }
     }
+
     void GenerateGrid()
     {
         for (int x = 0; x < width; x++)
@@ -76,6 +76,8 @@ public class GameManager : MonoBehaviour
         var center = new Vector2((float)width / 2 - 0.5f, (float)height / 2 - 0.5f);
         Camera.main.transform.position = new Vector3(center.x, center.y, -10);
 
+
+
     }
 
     void GameOver()
@@ -89,6 +91,7 @@ public class GameManager : MonoBehaviour
         {
             return false;
         }
+
         return true;
     }
 
@@ -101,9 +104,6 @@ public class GameManager : MonoBehaviour
 
         return true;
     }
-    
-    
-    
 
     //랜덤 블락 고른후에 대입. 
     bool CanSpawn(Vector3[] ShapePos)
@@ -115,69 +115,100 @@ public class GameManager : MonoBehaviour
                 int count = 0;
                 for (int k = 0; k < ShapePos.Length; k++)
                 {
-                    Vector3 CurShapePos = ShapePos[i] + new Vector3(i, j, 0);
-                    if(!InRange((int)CurShapePos.x,(int)CurShapePos.y)) break;
-                    if(!Possible((int)CurShapePos.x,(int)CurShapePos.y)) break;
+                    Vector3 CurShapePos = ShapePos[k] + new Vector3(i, j, 0);
+                    if (!InRange((int)CurShapePos.x, (int)CurShapePos.y)) break;
+                    if (!Possible((int)CurShapePos.x, (int)CurShapePos.y)) break;
                     ++count;
                 }
 
                 if (count == ShapePos.Length)
                 {
-    //              GoodPos.Add(new Vector3(i,j,0));
                     return true;
                 }
             }
         }
-        
+
         return false;
     }
-    //만약 값이 false면 게임 오버.
 
 
-    void SpawnBlock()
-
+    /*
+    bool IsValid(Vector3[] ShapePos)
     {
-        /*all check
-            if 오른쪽 왼쪽 다 0 이면 그자리에 Instantiate
-            생성후 1로 바꿔주기.
-            */
-
-        int random = Random.Range(0, Blocks.Length);
-
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-
-                var ShapePos = Blocks[random].GetComponent<Block>().ShapePos;
-                List<Vector3> GoodPos = new List<Vector3>();
-                
+                int count = 0;
                 for (int k = 0; k < ShapePos.Length; k++)
                 {
-                    Vector3 CurShapePos = ShapePos[i] + new Vector3(i, j, 0);
-                    if(!InRange((int)CurShapePos.x,(int)CurShapePos.y)) break;
-                    if(!Possible((int)CurShapePos.x,(int)CurShapePos.y)) break;
-                    
-                    GoodPos.Add(ShapePos[0]+new Vector3(i,j,0));
+                    Vector3 CurShapePos = ShapePos[k] + new Vector3(i,j,0);
+                    if (!InRange((int)CurShapePos.x, (int)CurShapePos.y)) break;
+                    if (!Possible((int)CurShapePos.x, (int)CurShapePos.y)) break;
+                    ++count;
                 }
 
-                
+                if (count == ShapePos.Length)
+                {
+                    return true;
+                }
             }
-
         }
-        
-        var Proper = GoodPos[0];
-        var Pos = Blocks[random].GetComponent<Block>().ShapePos;
-        Instantiate(Blocks[random], Proper, Quaternion.identity);
-        for (int i = 0; i < Pos.Length; i++)
+        return false;
+    }
+    */
+
+    void SpawnBlock()
+    {
+        int random = Random.Range(0, Blocks.Length);
+        var RandomBlock = Blocks[random].GetComponent<Block>().ShapePos;
+        for (int x = 0; x < width; x++)
         {
-            Vector3 SumPos = Pos[i] + Proper;
-            Array[(int)SumPos.x, (int)SumPos.y] = 1;
-        }
+            for (int j = 0; j < height; j++)
+            {
+                Vector3 vaildPos = new Vector3(x, j, 0);
 
+                if (!IsValidPosition(RandomBlock,vaildPos))
+                {
+                    Instantiate(Blocks[random], vaildPos, Quaternion.identity);
+                }
+
+                for (int i = 0; i < RandomBlock.Length; i++)
+                {
+                    Vector3 Save = RandomBlock[i] + vaildPos;
+                    Array[(int)Save.x, (int)Save.y] = 1;
+                }
+            }
+        }
 
     }
 
+    public bool IsValidPosition(Vector3[] ShapePos, Vector3 pos)
+    {
 
+        // The position is only valid if every cell is valid
+        for (int i = 0; i < ShapePos.Length; i++)
+        {
+            Vector3 Position = ShapePos[i] + pos;
 
+            if (!InRange((int)Position.x, (int)Position.y))
+            {
+                return false;
+            }
+            
+            if (!Possible((int)Position.x, (int)Position.y))
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
 }
+
+
+
+
+
+
